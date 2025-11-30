c8f3241f7a1be8279d2e706214f7c1701f55c13e
oluwafemi <oluwafemidojumo@gmail.com>
2025-11-30 01:23:10 +0100

    to-do-list

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    function addTask (){
+        const taskText = taskInput.value.trim();
+
+        // Validate empty input
+        if (taskText === ''){
+            alert('Enter a new task')
+            return;
+        }
+
+        // Task Creation and Removal
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        document.createButton()
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.className = 'remove-btn'
+
+        removeButton.onclick = () => {
+            taskList.removeChild(li)
+        };
+
+        li.appendChild(removeBtn);
+        taskList.appendChild(li);
+
+        // clear task input
+        taskInput.value = '';
+    }
+
+    // Attach event listeners
+    addButton.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    })
+
+    addTask();
+
+
+})
